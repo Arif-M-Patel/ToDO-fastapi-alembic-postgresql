1a9e13ac79d56565b688d7709f187db2f667e7c2
Arif Mustafa Patel <99581954+Arif-M-Patel@users.noreply.github.com>
2024-02-16 01:35:25 +0530

    added alembic folder

---
+++ alembic/versions/37a0d1838bb7_tasks_are_added_to_table.py
@@ -0,0 +1,33 @@
+"""tasks are added to table
+
+Revision ID: 37a0d1838bb7
+Revises: 968ede56ba65
+Create Date: 2024-02-16 00:12:55.568174
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '37a0d1838bb7'
+down_revision: Union[str, None] = '968ede56ba65'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    # Insert initial data
+    op.execute("""
+        INSERT INTO todos (title, description, completed) 
+        VALUES ('task 1', 'this is first task', false), 
+               ('task 2', 'this is second task', false)
+    """)
+
+def downgrade():
+    # Delete inserted data
+    op.execute("""
+        DELETE FROM todos WHERE id IN (1, 2)
+    """)
