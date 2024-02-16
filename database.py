99f2922a0723f11aa1cbc6e9ba812b9fbdf5ea36
Arif Mustafa Patel <99581954+Arif-M-Patel@users.noreply.github.com>
2024-02-16 12:53:42 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,13 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# Database URL (replace with your PostgreSQL database URL)
+# Database URL 
 DATABASE_URL = "postgresql://postgres:Arif@localhost/todo_db"
 
-# Create a SQLAlchemy database engine
+# database engine
 engine = create_engine(DATABASE_URL)
 
-# Create a sessionmaker to manage database sessions
+#  sessionmaker to manage database sessions
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
